3ca7137f8c36627afce5a534cf8e96baa3181ab3
Raul Arturo Salomon Iglesias <73568954+RaulSalomonIgl@users.noreply.github.com>
2022-05-15 14:01:23 -0500

    Cargando archivos src a Master

---
+++ src/views/SignUpView.vue
@@ -0,0 +1,67 @@
+<template>
+
+<div class="body-sign">
+    <div class="form_sign" id="signUp">
+
+        <h1>sign up</h1>
+
+        <div class="row g-2">
+            <div class="col-md">
+                <div class="form-floating">
+                    <input type="text" class="form-control" id="floatingInputGrid">
+                    <label for="floatingInputGrid">Name</label>
+                </div>
+            </div>
+
+            <div class="col-md">
+                <div class="form-floating">
+                    <input type="text" class="form-control" id="floatingInputGrid">
+                    <label for="floatingInputGrid">Last Name</label>
+                </div>
+            </div>
+
+            <!-- Aclaración: Validar Metodos de obtencion de apellidos-->
+            <div class="col-md"> 
+                <div class="form-floating">
+                    <input type="text" class="form-control" id="floatingInputGrid">
+                    <label for="floatingInputGrid">2nd Last Name</label>
+                </div>
+            </div>
+
+        </div>
+        
+
+        <!-- Credenciales -->
+        <div class="col-md">
+            <div class="form-floating">
+                <input type="text" class="form-control" id="floatingInputGrid">
+                <label for="floatingInputGrid">Username</label>
+            </div>
+        </div>
+
+        <div class="row g-2">
+            <div class="col-md">
+                <div class="form-floating">
+                    <input type="email" class="form-control" id="floatingInputGrid"  placeholder="name@example.com">
+                    <label for="floatingInputGrid">Email</label>
+                </div>
+            </div>
+
+            <div class="col-md">
+                <div class="form-floating">
+                    <input type="password" class="form-control" id="floatingInputGrid">
+                    <label for="floatingInputGrid">Password</label>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-auto">
+            <button type="submit" class="btn btn-login mb-3">Confirm identity</button>
+        </div>
+
+        <router-link to="/login" class="register">i have account</router-link>
+    </div>
+
+    </div>
+</template>
+
